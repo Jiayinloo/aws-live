9aeda0994695d8241fffc638e4d31845b99484eb
Jiayin Loo <99061013+Jiayinloo@users.noreply.github.com>
2022-09-25 13:00:28 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -22,9 +22,9 @@ table = 'employee'
 
 
 @app.route("/", methods=['GET', 'POST'])
-def home():
-    return render_template('AddEmp.html')
-
+def employee():
+    # return render_template('AddEmp.html')
+    return render_template('employee.html')
 
 @app.route("/about", methods=['POST'])
 def about():
@@ -42,6 +42,7 @@ def AddEmp():
     email = request.form['email']
     phone = request.form['phone']
     emp_image_file = request.files['emp_image_file']
+    emp_resume = request.files['emp_resume']
     emp_certificate = request.files['emp_certificate']
 
     insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %d, %d, %s, %s)"
@@ -50,6 +51,12 @@ def AddEmp():
     if emp_image_file.filename == "":
         return "Please select a file"
 
+    if emp_resume == "":
+        return "Please select a file"
+    
+    if emp_certificate == "":
+        return "Please select a file"
+    
     try:
 
         cursor.execute(insert_sql, (emp_id, first_name, last_name, position, salary, ot, email, phone))
@@ -57,12 +64,14 @@ def AddEmp():
         emp_name = "" + first_name + " " + last_name
         # Uplaod image file in S3 #
         emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
+        emp_resume_name_in_s3 = "emp-id-" + str(emp_id) + "_resume"
         emp_certificate_name_in_s3 = "emp-id-" + str(emp_id) + "_certificate"
         s3 = boto3.resource('s3')
 
         try:
             print("Data inserted in MySQL RDS... uploading image to S3...")
             s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
+            s3.Bucket(custombucket).put_object(Key=emp_resume_name_in_s3, Body=emp_resume)
             s3.Bucket(custombucket).put_object(Key=emp_certificate_name_in_s3, Body=emp_certificate)
             bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
             s3_location = (bucket_location['LocationConstraint'])
@@ -76,6 +85,7 @@ def AddEmp():
                 s3_location,
                 custombucket,
                 emp_image_file_name_in_s3,
+                emp_resume_name_in_s3,
                 emp_certificate_name_in_s3)
 
         except Exception as e:
@@ -85,7 +95,7 @@ def AddEmp():
         cursor.close()
 
     print("all modification done...")
-    return render_template('AddEmpOutput.html', name=emp_name)
+    return render_template('employee.html', name=emp_name)
 
 
 if __name__ == '__main__':
